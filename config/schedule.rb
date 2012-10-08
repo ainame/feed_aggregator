539fdd49b43e7b9aeccc394f272bf57e7b198298
ainame <ainame954@facebook.com>
2012-10-09 00:32:27 +0900

    fix scheduler.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -19,6 +19,6 @@
 
 # Learn more: http://github.com/javan/whenever
 
-every 30.minutes do
-  runner "Tasks::UpdateFeedsTask.insert_feed_and_update_tags"
+every 10.minutes do
+  runner "Tasks::UpdateFeedsTask.new.insert_feed_and_update_tags"
 end
